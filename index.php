c4564bbd80d40b90a5896101626f07ba26c28763
NandiniSHinduja <69416051+NandiniSHinduja@users.noreply.github.com>
2020-10-04 01:37:10 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<h1> Hello Everybody! </h1>
+<?php
+$x=23;
+$y=12;
+$z=$x+$y;
+echo "This is my first PHP application \n";
+echo "The sum of $x and $y is $z \n";
+if($x<$y)
+{
+  echo "$y is larger than $x \n";
+}
+else
+{
+  echo "$x is larger than $y \n";
+}
+?>
+<pre> PHP was inspired from Perl </pre>
+<pre> It was also inspired from C </pre>
